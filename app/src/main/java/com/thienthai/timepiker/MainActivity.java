5d03cd3fd587f28767afe1ec8c5b2b111a05e469
thienthai-hub <vohuyenthienthai94@gmail.com>
2021-07-30 18:51:58 +0700

    Time Picker

---
+++ app/src/main/java/com/thienthai/timepiker/MainActivity.java
@@ -0,0 +1,24 @@
+package com.thienthai.timepiker;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.app.TimePickerDialog;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.TimePicker;
+
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+    }
+
+}
